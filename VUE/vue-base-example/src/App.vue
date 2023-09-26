b9e24888bf553f30802e27933061dc775daef996
Lucia Karlen <luuukarlen@gmail.com>
2023-09-25 22:21:01 -0300

    ejercicio1

---
+++ VUE/vue-base-example/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Ejercicio1/>
+  </div>
+</template>
+
+<script>
+import ExampleComponent from './components/ExampleComponent'
+import Ejercicio1 from './components/Ejercicio1'
+
+export default {
+  name: 'App',
+  components: {
+    Ejercicio1,
+    ExampleComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
